24c3ac3117bb4a2b82eb04710700960e7af3c7ae
Bora Mici <BMici@optimaalbania.com>
2023-04-24 14:50:08 +0200

    last commit

--- src/main/java/com/example/webapplication/Email/EmailService.java
+++ src/main/java/com/example/webapplication/Email/EmailService.java
@@ -1,74 +1,70 @@
-//package com.example.webapplication.Email;
-//
-//import com.example.webapplication.user.entity.User;
-//import com.sun.mail.smtp.SMTPTransport;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//import javax.mail.*;
-//import javax.mail.internet.AddressException;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-//import java.util.Date;
-//import java.util.Properties;
-//
-//@Service
-//public class EmailService {
-//    private final JavaMailSender mailSender;
-//
-//    public EmailService(JavaMailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
-//
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.port}")
-//    private String port;
-//
-//    @Value("${spring.mail.username}")
-//    private String userName;
-//
-//    @Value("${spring.mail.password}")
-//    private String passwordP;
-//    public void sendReminderEmail(User user){
-//        final String username = userName;
-//        final String password = passwordP;
-//
-//        Properties prop = new Properties();
-//        prop.put("mail.smtp.host", host);
-//        prop.put("mail.smtp.port", port);
-//        prop.put("mail.smtp.auth", "true");
-//        prop.put("mail.smtp.starttls.enable", "true"); //TLS
-//
-//        Session session = Session.getInstance(prop,
-//                new javax.mail.Authenticator() {
-//                    protected PasswordAuthentication getPasswordAuthentication() {
-//                        return new PasswordAuthentication(username, password);
-//                    }
-//                });
-//
-//        try {
-//
-//            Message message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress(username));
-//            message.setRecipients(
-//                    Message.RecipientType.TO,
-//                    InternetAddress.parse(user.getEmail())
-//            );
-//            message.setSubject("Testing Gmail TLS");
-//            message.setText("Dear Mail Crawler,"
-//                    + "\n\n Please do not spam my email!");
-//
-//            Transport.send(message);
-//
-//            System.out.println("Done");
-//
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
+package com.example.webapplication.Email;
+
+import com.example.webapplication.user.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private String port;
+
+    @Value("${spring.mail.username}")
+    private String userName;
+
+    @Value("${spring.mail.password}")
+    private String passwordP;
+    public void sendReminderEmail(User user){
+        final String username = userName;
+        final String password = passwordP;
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", host);
+        prop.put("mail.smtp.port", port);
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(user.getEmail())
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
